c50e1bef97e038dcd3cc6b1f3c43c6e7cd383743
pje39613 <joss.whittle@rfi.ac.uk>
2022-05-04 15:09:33 +0100

    Test with srun --mpi

--- example/job.sh
+++ example/job.sh
@@ -32,4 +32,4 @@ export CONTAINER="docker://quay.io/rosalindfranklininstitute/jax@sha256:0c9fcac6
 srun --mpi list
 
 # Execute the parallel job
-srun singularity run --nv $CONTAINER python example/job.py --log_nvsmi
+srun --mpi singularity run --nv $CONTAINER python example/job.py --log_nvsmi
