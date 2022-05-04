1ab45eca1ee50215db566e9081f47cb71f8f4ac4
pje39613 <joss.whittle@rfi.ac.uk>
2022-05-04 20:29:27 +0100

    #SBATCH --nodes 2 #SBATCH --ntasks-per-node 2 #SBATCH --cpus-per-task 36 #SBATCH --gpus-per-task 2

--- example/job.sh
+++ example/job.sh
@@ -7,7 +7,7 @@
 #SBATCH --nodes 2
 #SBATCH --ntasks-per-node 2
 #SBATCH --cpus-per-task 36
-#SBATCH --gpus-per-task 1
+#SBATCH --gpus-per-task 2
 
 # Load a bare-bones environment that will support CUDA enabled MPI within singularity containers
 module purge
