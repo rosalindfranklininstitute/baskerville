97b702e06d8b1d95817f5036d39036a9743edba0
pje39613 <joss.whittle@rfi.ac.uk>
2022-05-04 05:27:06 +0100

    Add batch dims

--- example/job.py
+++ example/job.py
@@ -57,10 +57,10 @@ def task(argv, logger, MPI):
 
     logger.info(f'Training dataset [{FLAGS.train_dataset}] with length [{len(hub_ds_train)}]')
     ds_train = hub_ds_train.tensorflow()
-    ds_train = ds_train.map(preprocess_fn, num_parallel_calls=tf.data.AUTOTUNE)
-    # .shuffle(len(hub_ds_train), seed=FLAGS.train_dataset_shuffle_seed, reshuffle_each_iteration=True) \
-    # .batch(FLAGS.train_batch_size, drop_remainder=True, num_parallel_calls=tf.data.AUTOTUNE) \
-    # .batch(jax.local_device_count(), drop_remainder=True, num_parallel_calls=tf.data.AUTOTUNE)
+    ds_train = ds_train.map(preprocess_fn, num_parallel_calls=tf.data.AUTOTUNE) \
+                .shuffle(len(hub_ds_train), seed=FLAGS.train_dataset_shuffle_seed, reshuffle_each_iteration=True) \
+                .batch(FLAGS.train_batch_size, drop_remainder=True, num_parallel_calls=tf.data.AUTOTUNE) \
+                .batch(jax.local_device_count(), drop_remainder=True, num_parallel_calls=tf.data.AUTOTUNE)
 
     hub_ds_val = hub.load(FLAGS.val_dataset, read_only=True,
                           memory_cache_size=FLAGS.val_dataset_mem_cache,
@@ -68,9 +68,9 @@ def task(argv, logger, MPI):
 
     logger.info(f'Validation dataset [{FLAGS.val_dataset}] with length [{len(hub_ds_val)}]')
     ds_val   = hub_ds_val.tensorflow()
-    ds_val   = ds_val.map(preprocess_fn, num_parallel_calls=tf.data.AUTOTUNE)
-    # .batch(FLAGS.val_batch_size, drop_remainder=False, num_parallel_calls=tf.data.AUTOTUNE) \
-    # .batch(jax.local_device_count(), drop_remainder=False, num_parallel_calls=tf.data.AUTOTUNE)
+    ds_val   = ds_val.map(preprocess_fn, num_parallel_calls=tf.data.AUTOTUNE) \
+                .batch(FLAGS.val_batch_size, drop_remainder=False, num_parallel_calls=tf.data.AUTOTUNE) \
+                .batch(jax.local_device_count(), drop_remainder=False, num_parallel_calls=tf.data.AUTOTUNE)
 
     for index, batch in zip(range(5), iter(ds_train)):
 
