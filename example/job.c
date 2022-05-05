117ec69a48f6f830326c4aceee543eada5281a50
pje39613 <joss.whittle@rfi.ac.uk>
2022-05-05 20:19:58 +0100

    Create minimal MPI C example to test srun without containers and mpi4py

---
+++ example/job.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+#include <mpi.h>
+
+#define MAX_HEADER_LEN 256
+
+int main(int argc, char *argv[]) {
+    MPI_Init(&argc, &argv);
+
+    int WORLD_RANK, WORLD_SIZE, LOCAL_RANK, LOCAL_SIZE;
+    MPI_Comm_rank(MPI_COMM_WORLD, &WORLD_RANK);
+    MPI_Comm_size(MPI_COMM_WORLD, &WORLD_SIZE);
+    MPI_Comm comm_local;
+    MPI_Comm_split_type(MPI_COMM_WORLD, MPI_COMM_TYPE_SHARED, 0, MPI_INFO_NULL, &comm_local);
+    MPI_Comm_rank(comm_local, &LOCAL_RANK);
+    MPI_Comm_size(comm_local, &LOCAL_SIZE);
+
+    char LOG_HEADER[MAX_HEADER_LEN];
+    sprintf(&LOG_HEADER, "W %03d:%03d | L %03d:%03d", WORLD_RANK, WORLD_SIZE, LOCAL_RANK, LOCAL_SIZE);
+
+    printf("%s | Starting...\n", LOG_HEADER);
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    int *xs = (int*) malloc(sizeof(int) * WORLD_SIZE);
+
+    for (int i = 0; i < WORLD_SIZE; i++) {
+        if (i == WORLD_RANK) {
+            printf("%s | BCAST ROOT", LOG_HEADER);
+            for (int j = 0; j < WORLD_SIZE; j++) {
+                xs[j] = WORLD_RANK;
+                printf(" %d", xs[j]);
+            }
+            printf("\n");
+        }
+
+        MPI_Bcast(xs, WORLD_SIZE, MPI_INT, i, MPI_COMM_WORLD);
+
+        for (int j = 0; j < WORLD_SIZE; j++) {
+            MPI_Barrier(MPI_COMM_WORLD);
+            if (j == WORLD_RANK) {
+                printf("%s | BCAST", LOG_HEADER);
+                for (int k = 0; k < WORLD_SIZE; k++) {
+                    xs[k] = WORLD_RANK;
+                    printf(" %d", xs[k]);
+                }
+                printf("\n");
+            }
+        }
+
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    free(xs);
+
+    printf("%s | Halting...\n", LOG_HEADER);
+
+    MPI_Finalize();
+    return 0;
+}
--- example/job.sh
+++ example/job.sh
@@ -3,13 +3,13 @@
 #SBATCH --qos rfi
 #SBATCH --mail-user joss.whittle@rfi.ac.uk
 #SBATCH --mail-type ALL
-#SBATCH --time 20
+#SBATCH --time 2
 #SBATCH --nodes 1
 #SBATCH --ntasks-per-node 1
 #SBATCH --cpus-per-task 36
 #SBATCH --gpus-per-node 0
 
-# Load a bare-bones environment that will support CUDA enabled MPI within singularity containers
+# Load a bare-bones environment that will support CUDA enabled MPI
 module purge
 module load baskerville
 module load bask-apps/live
@@ -18,18 +18,8 @@ module load OpenMPI/4.0.5-gcccuda-2020b
 # Enable verbose logging of job script commands
 set -x
 
-# Root directory for project storage (update this to a directory you control)
-export PROJECT_DIR="/bask/projects/f/ffnr0871-rfi-test/pje39613"
-
-# Place singularity cache dir in project storage since /home/ is limited to 20GB per user
-# Singularity can use up a LOT of cache space when converting OCI images to singularity images!
-export SINGULARITY_CACHEDIR="$PROJECT_DIR/.singularity-cache"
-
-# Use container uri with hash digest so that the correct container version gets used even if the job queues
-# for a long time and you have pushed a newer version of the container in the meantime for future experiments
-export CONTAINER="docker://quay.io/rosalindfranklininstitute/jax@sha256:0c9fcac6a84d3c427e6b92489452afb46157995996524d40c1a4286c7ca6bb49"
+# Compile the job
+mpicc job.c -o job
 
 # Execute the parallel job
-mpirun singularity run --nv $CONTAINER python example/job.py --log_nvsmi \
-                       --train_dataset "$PROJECT_DIR/mnist-train" \
-                       --val_dataset   "$PROJECT_DIR/mnist-test"
+srun job
