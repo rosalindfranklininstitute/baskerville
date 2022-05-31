309070923341c4303af27b450d67d310da4473b0
pje39613 <joss.whittle@rfi.ac.uk>
2022-05-31 18:04:23 +0100

    #SBATCH --nodes 1 #SBATCH --ntasks-per-node 1 #SBATCH --cpus-per-task 1 #SBATCH --gpus-per-task 0
    Revert to srun

--- example/job.sh
+++ example/job.sh
@@ -30,4 +30,4 @@ export SINGULARITY_CACHEDIR="$PROJECT_DIR/.singularity-cache"
 export CONTAINER="docker://quay.io/rosalindfranklininstitute/jax@sha256:0c9fcac6a84d3c427e6b92489452afb46157995996524d40c1a4286c7ca6bb49"
 
 # Execute the parallel job
-mpirun singularity run --nv $CONTAINER python example/job.py --log_nvsmi --log_env
+srun singularity run --nv $CONTAINER python example/job.py --log_nvsmi --log_env
